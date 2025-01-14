43f2f511724db563af30168f2b84ddc53976342f
ronja <ronja.syrjala@gmail.com>
2025-01-14 18:40:59 +0200

    fix

--- client/src/App.jsx
+++
@@ -1,8 +0,0 @@
-
-function App() {
-  return (
-    <div>dffd</div>
-  )
-}
-
-export default App
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
 import { BrowserRouter, Routes, Route } from "react-router";
 import Todos from './components/Todos.jsx';
 import  Login  from './components/Login.jsx';
